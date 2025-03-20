690f42e4b6c13861960ed4f70e39458b865b5b75
IM23a-loritzg <loritzg@bzz.ch>
2025-03-20 11:29:04 +0100

    Update

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,13 +17,13 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    return (
+        <html lang="en">
+        <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
         {children}
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
