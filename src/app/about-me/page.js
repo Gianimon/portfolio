ebdda5fba11b14247bd9fd5ab4b5e210da197acb
IM23a-loritzg <loritzg@bzz.ch>
2025-05-08 11:30:06 +0200

    Update 8.5.2025

--- src/app/about-me/page.js
+++ src/app/about-me/page.js
@@ -49,7 +49,6 @@ export default function AboutMe() {
                 </div>
                 <div><Carousel></Carousel></div>
 
-
                 <div className="flex flex-col items-center py-10">
                     <div className="text-3xl mb-20">
                         Skills
